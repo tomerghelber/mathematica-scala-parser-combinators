1c982a0bd99cca2836cb4f158974d5ad758dc468
Tomer Ghelber <tomergelber@gmail.com>
2019-10-31 23:01:10 +0200

    Wrote a lot of parsers

--- src/main/scala/tomerghelber/mathematica/ast/ASTNode.scala
+++ src/main/scala/tomerghelber/mathematica/ast/ASTNode.scala
@@ -3,26 +3,86 @@ package tomerghelber.mathematica.ast
 trait ASTNode
 
 trait NumberNode extends ASTNode
+trait ScriptNode extends ASTNode
 
 case class IntegerNode(value: BigInt) extends NumberNode
-
-/**
- * A node for a parsed floating number string. The floating point string is not
- * converted to a binary float or double type. only the String representation is
- * stored.
- */
 case class FloatNode(value: Double) extends NumberNode
-
-/**
- * A node for a parsed fraction string
- */
-case class FractionNode(numerator: IntegerNode, denominator: IntegerNode) extends NumberNode
-
-/**
- * A list of <code>ASTNode</code>'s which represents a parsed function.
- */
-case class FunctionNode(symbol: SymbolNode, args: Seq[ASTNode]) extends ASTNode
-
 case class StringNode(value: String) extends ASTNode
-
 case class SymbolNode (value: String) extends ASTNode
+
+case class MessageNameNode(symbol: ASTNode, tag: StringNode) extends ASTNode
+case class OverscriptNode(expr1: ASTNode, expr2: ASTNode) extends ScriptNode
+case class UnderscriptNode(expr1: ASTNode, expr2: ASTNode) extends ScriptNode
+case class SubscriptNode(expr1: ASTNode, expr2: ASTNode) extends ScriptNode
+
+case class PartNode(expr: ASTNode, parts: List[ASTNode]) extends ASTNode
+
+case class IncrementNode(expr: ASTNode) extends ASTNode
+case class DecrementNode(expr: ASTNode) extends ASTNode
+case class PreincrementNode(expr: ASTNode) extends ASTNode
+case class PredecrementNode(expr: ASTNode) extends ASTNode
+case class CompositionNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class RightCompositionNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class MapNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class MapAllNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class Apply2Node(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class Apply3Node(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class FactorialNode(expr: ASTNode) extends ASTNode
+case class Factorial2Node(expr: ASTNode) extends ASTNode
+case class ConjugateNode(expr: ASTNode) extends ASTNode
+case class TransposeNode(expr: ASTNode) extends ASTNode
+case class ConjugateTransposeNode(expr: ASTNode) extends ASTNode
+case class DerivativeNode(num: Int, expr: ASTNode) extends ASTNode
+case class StringJoinNode(expr1: ASTNode, expr3: ASTNode, expr2: ASTNode) extends ASTNode
+case class PowerNode(base: ASTNode, exponent: ASTNode) extends ASTNode
+case class SqrtNode(expr: ASTNode) extends ASTNode
+case class DifferentialDNode(expr: ASTNode) extends ASTNode
+case class DNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class DelNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class DiscreteShiftNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class DiscreteRatioNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class DifferenceDeltaNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class CrossNode(expr1: ASTNode, expr3: ASTNode, expr2: ASTNode) extends ASTNode
+case class DotNode(expr1: ASTNode, expr3: ASTNode, expr2: ASTNode) extends ASTNode
+case class PlusMinusNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class SinglePlusMinusNode(expr: ASTNode) extends ASTNode
+case class MinusPlusNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class SingleMinusPlusNode(expr: ASTNode) extends ASTNode
+case class DivideNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class TimesNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class PlusNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class IntegrateNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class IntersectionNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class UnionNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+
+case class EqualNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class UnequalNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class GreaterNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class GreaterEqualNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class LessNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class LessEqualNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+
+case class SameQNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class UnSameQNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class ElementNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class NotElementNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class SubsetNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class SupersetNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class ForAllNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class ExistsNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class NotExistsNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class NotNode(expr: ASTNode) extends ASTNode
+case class AndNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class NandNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class XorNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class XnorNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class OrNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class NorNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class EquivalentNode(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+case class Implies(expr1: ASTNode, expr2: ASTNode) extends ASTNode
+
+case class LimitNode(expr1: ASTNode, expr3: ASTNode, expr2: ASTNode) extends ASTNode
+case class MaxLimitNode(expr1: ASTNode, expr3: ASTNode, expr2: ASTNode) extends ASTNode
+case class MinLimitNode(expr1: ASTNode, expr3: ASTNode, expr2: ASTNode) extends ASTNode
+
+case class SpanNode(expr1: ASTNode, expr3: ASTNode, expr2: ASTNode) extends ASTNode
--- src/main/scala/tomerghelber/mathematica/parser/MathematicaParser.scala
+++ src/main/scala/tomerghelber/mathematica/parser/MathematicaParser.scala
@@ -20,7 +20,7 @@ class MathematicaParser() extends StdTokenParsers {
 //  lexical.reserved ++= List("*", "-", "+", "/", "!")
   lexical.delimiters ++= List("(", ")", "[", "]", ",", "*", "-", "+", "/", "!", "=", "^")
 
-  def number: Parser[NumberNode] = numericLit ^^ { n =>
+  private def number: Parser[NumberNode] = numericLit ^^ { n =>
     try {
       IntegerNode(BigInt(n))
     } catch {
@@ -28,69 +28,216 @@ class MathematicaParser() extends StdTokenParsers {
     }
   }
 
-  def symbol: Parser[SymbolNode] = ident ^^ SymbolNode
+  private def symbol: Parser[SymbolNode] = ident ^^ SymbolNode
 
-  def applyFunction: Parser[FunctionNode] = symbol ~ ("(" ~> repsep(mathExpression, ",") <~ ")") ^^ {
-    case functionName ~ arguments => FunctionNode(functionName, arguments)
+  private def terminal = number | symbol
+
+  private def lower: Parser[ASTNode] = terminal | "(" ~> root <~ ")"
+
+//  private def overscriptAndUnderscript: Parser[ASTNode] = lower ~ ("\\+" | "\\&") ~ lower ^^ {
+//    case expr1 ~ "\\&" ~ expr2 => OverscriptNode(expr1, expr2)
+//    case expr1 ~ "\\+" ~ expr2 => UnderscriptNode(expr1, expr2)
+//  } | lower
+//
+//  private def subscript: Parser[ASTNode] = overscriptAndUnderscript ~ "\\_" ~ overscriptAndUnderscript ^^ {
+//    case expr1 ~ "\\+" ~ expr2 => SubscriptNode(expr1, expr2)
+//  } | overscriptAndUnderscript
+//
+//  private def part: Parser[ASTNode] = subscript ~ (("[" ~> rep1sep(subscript, ",") <~ "]") | ("[[" ~> rep1sep(subscript, ",") <~ "]]") | ("〚" ~> rep1sep(subscript, ",") <~ "〛")) ^^ {
+//    case expr ~ parts => PartNode(expr, parts)
+//  } | subscript
+//
+//  private def incementAndDecrement: Parser[ASTNode] = part ~ ("++"|"--") ^^ {
+//    case expr ~ "++" => IncrementNode(expr)
+//    case expr ~ "--" => DecrementNode(expr)
+//  } | part
+//
+//  private def preincementAndPredecrement: Parser[ASTNode] = ("++"|"--") ~ incementAndDecrement ^^ {
+//    case "++" ~ expr => PreincrementNode(expr)
+//    case "--" ~ expr => PredecrementNode(expr)
+//  } | incementAndDecrement
+//
+//  private def composition: Parser[ASTNode] = preincementAndPredecrement ~ ("@*" | "/*") ~ preincementAndPredecrement ^^ {
+//    case expr1 ~ "@*" ~ expr2 => CompositionNode(expr1, expr2)
+//    case expr1 ~ "/*" ~ expr2 => RightCompositionNode(expr1, expr2)
+//  } | preincementAndPredecrement
+//
+//  private def mapAndApply: Parser[ASTNode] = composition ~ ("/@" | "//@" | "@@" | "@@@") ~ composition ^^ {
+//    case expr1 ~ "/@" ~ expr2 => MapNode(expr1, expr2)
+//    case expr1 ~ "//@" ~ expr2 => MapAllNode(expr1, expr2)
+//    case expr1 ~ "@@" ~ expr2 => Apply2Node(expr1, expr2)
+//    case expr1 ~ "@@@" ~ expr2 => Apply3Node(expr1, expr2)
+//  } | composition
+
+  private def factorial: Parser[ASTNode] = lower ~ opt("!" ~ opt("!")) ^^ {
+    case expr ~ None => expr
+    case expr ~ Some("!" ~ None) => FactorialNode(expr)
+    case expr ~ Some("!" ~ Some("!")) => Factorial2Node(expr)
   }
 
-  def terminal = number | symbol
+//  private def conjugateAndTranspose: Parser[ASTNode] = factorial ~ ("\uF3C8" | "\uF3C7" | "\uF3C9" | "\uF3CE") ^^ {
+//    case expr ~ "\uF3C8" => ConjugateNode(expr)
+//    case expr ~ "\uF3C7" => TransposeNode(expr)
+//    case expr ~ ("\uF3C9"|"\uF3CE") => ConjugateTransposeNode(expr)
+//  } | factorial
+//
+//  private def derivative: Parser[ASTNode] = conjugateAndTranspose ~ rep("'") ^^ {
+//    case expr ~ number => DerivativeNode(number.size, expr)
+//  }
+//
+//  private def stringJoin: Parser[ASTNode] = derivative ~ ("<>" ~> derivative <~ "<>") ~ derivative ^^ {
+//    case expr1 ~ expr2 ~ expr3 => StringJoinNode(expr1, expr2, expr3)
+//  } | derivative
 
-  def lower = applyFunction | terminal | "(" ~> mathExpression <~ ")"
+  private def power: Parser[ASTNode] = rep1sep(factorial, "^") ^^ (values => values.reduceRight(PowerNode))
 
-  def factorial = lower ~ rep("!") ^^ {
-    case expr ~ factorialSign =>
-      factorialSign.foldLeft(expr)((a, _) => FunctionNode(SymbolNode("Factorial"), Seq(a)))
-  }
+//  // TODO: create this one
+//  private def verticalArrowAndVectorOperators: Parser[ASTNode] = power
+//
+//  private def sqrt: Parser[ASTNode] = "\\@" ~ verticalArrowAndVectorOperators ^^ {
+//    case "\\@" ~ expr => SqrtNode(expr)
+//  } | verticalArrowAndVectorOperators
+//
+//  private def differentialD: Parser[ASTNode] = "d" ~> sqrt ^^ {
+//    expr => DifferentialDNode(expr)
+//  } | sqrt
+//
+//  private def discreteOperators: Parser[ASTNode] = ("∂" | "∇" | "\uF4A3" | "\uF4A5" | "\uF4A4") ~ differentialD ~ differentialD ^^ {
+//    case "∂" ~ expr1 ~ expr2 => DNode(expr1, expr2)
+//    case  "∇" ~ expr1 ~ expr2 => DelNode(expr1, expr2)
+//    case "\uF4A3" ~ expr1 ~ expr2 => DiscreteShiftNode(expr1, expr2)
+//    case "\uF4A5" ~ expr1 ~ expr2 => DiscreteRatioNode(expr1, expr2)
+//    case "\uF4A4" ~ expr1 ~ expr2 => DifferenceDeltaNode(expr1, expr2)
+//  } | differentialD
+//
+//  private def squareAndCircle: Parser[ASTNode] = discreteOperators
+//
+//  private def cross: Parser[ASTNode] = squareAndCircle ~ ("\uF4A0" ~> squareAndCircle <~ "\uF4A0") ~ squareAndCircle ^^ {
+//    case expr1 ~ expr2 ~ expr3 => CrossNode(expr1, expr2 , expr3)
+//  } | squareAndCircle
+//
+//  private def dot: Parser[ASTNode] = cross ~ ("." ~> cross <~ ".") ~ cross ^^ {
+//    case expr1 ~ expr2 ~ expr3 => DotNode(expr1, expr2 , expr3)
+//  } | cross
+//
+//  private def signedExpression: Parser[ASTNode] = ("+" | "-" | "±" | "∓") ~ dot ^^ {
+//    case "+" ~ expr => expr
+//    case "-" ~ expr => TimesNode(IntegerNode(-1), expr)
+//    case "±" ~ expr => SinglePlusMinusNode(expr)
+//    case "∓" ~ expr => SingleMinusPlusNode(expr)
+//  } | dot
+//
+  private def divide: Parser[ASTNode] = (power <~ ("/" | "÷" | "\\/")) ~ power ^^ {
+    case expr1 ~ expr2 => DivideNode(expr1, expr2)
+  } | power
 
-  def power = rep1sep(factorial, "^") ^^ {
-    case elem :: Nil => elem
-    case firsts :+ beforeLast :+ last =>
-      val symbol = SymbolNode("Power")
-      firsts.foldLeft(FunctionNode(symbol, Seq(beforeLast, last)))((rhs, lhs) => FunctionNode(symbol, Seq(rhs, lhs)))
+  private def times: Parser[ASTNode] = rep1sep(divide, opt("*" | "×")) ^^ (values => values.reduceLeft(TimesNode))
+//
+//  private def product: Parser[ASTNode] = times
+//
+//  private def integrate: Parser[ASTNode] = "∫" ~> product ~ product ^^ {
+//    case expr1 ~ expr2 => IntegrateNode(expr1, expr2)
+//  } | product
+//
+//  //  private def sumAndLimit: Parser[ASTNode] = "∑" | ("\uF438" | "\uF439" | "\uF43A") ^^ {
+//  //    case "\uF438" ~ e3 => LimitNode(e3, e1, e2)
+//  //    case "\uF439" ~ e3 => MaxLimitNode(e3, e1, e2)
+//  //    case "\uF43A" ~ e3 => MinLimitNode(e3, e1, e2)
+//  //  } | integrate
 
-  }
-
-  def operatorToSymbol = Map(
-    ("+", SymbolNode("Plus")),
-    ("-", SymbolNode("Minus")),
-    ("*", SymbolNode("Times")),
-    ("/", SymbolNode("Div")),
-  )
-
-  def timesAndDiv = chainl1[ASTNode](power,
-    opt("*" | "/") ^^ {operator => (rhs, lhs) => FunctionNode(operatorToSymbol(operator.getOrElse("*")), Seq(rhs, lhs))}
-  )
-
-  def plusAndMinus = chainl1[ASTNode](timesAndDiv,
-    ("+" | "-") ^^ {operator => (rhs, lhs) => FunctionNode(operatorToSymbol(operator), Seq(rhs, lhs))}
-  )
-
-  def signedExpression = opt("-" | "+") ~ plusAndMinus ^^ {
-    case None ~ expressionToSign => expressionToSign
-    case Some(sign) ~ expressionToSign => FunctionNode(operatorToSymbol("*"), List(IntegerNode(BigInt(sign + "1")), expressionToSign))
-  }
-
-  // The root of all math expressions
-  def mathExpression: Parser[ASTNode] = signedExpression
-
-  // The root of all expressions
-  def expression = mathExpression
-
-  def set = (symbol <~ "=") ~ expression ^^ {
-    case name ~ value => FunctionNode(SymbolNode("Set"), Seq(name, value))
-  }
-
-  // The root of all statements
-  def statement = log(set | expression)("statment")
-
-  private def realParse(expressionString: String): ASTNode = {
-    val reader = new lexical.Scanner(expressionString)
-    phrase(statement)(reader) match {
-      case Success(result, _) => result
-      case noSuccess => throw SyntaxException(noSuccess.toString)
+  private def plusAndMinus: Parser[ASTNode] = {
+    val operatorToNodeCreator: String => ((ASTNode, ASTNode) => ASTNode) = {
+      case "+" => PlusNode
+      case "-" => (expr1, expr2) => PlusNode(expr1, TimesNode(IntegerNode(-1), expr2))
+      case "±" => PlusMinusNode
+      case "∓" => MinusPlusNode
+    }
+    rep(times ~ (("+" | "-" | "±" | "∓") ^^ operatorToNodeCreator)) ~ times ^^ {
+      case tuples ~ last =>
+        tuples.foldRight(last){case (n2 ~ creator, n1) => creator(n2, n1)}
     }
   }
+
+//  private def intersection: Parser[ASTNode] = (plusAndMinus <~ "⋂") ~ plusAndMinus ^^ {
+//    case expr1 ~ expr2 => IntersectionNode(expr1, expr2)
+//  } | plusAndMinus
+//
+//  private def union: Parser[ASTNode] = (intersection <~ "⋃") ~ intersection ^^ {
+//    case expr1 ~ expr2 => UnionNode(expr1, expr2)
+//  } | intersection
+//
+//  private def span: Parser[ASTNode] = union ~ (";;" ~> union <~ ";;") ~ union ^^ {
+//    case i ~ j ~ k => SpanNode(i, j, k)
+//  } | union
+//
+//  private def equalities: Parser[ASTNode] = span ~ ("==" | "\uF7D9" | "!=" | ">" | ">=" | "≥" | "⩾" | "<" | "<=" | "≤" | "⩽") ~ span ^^ {
+//    case expr1 ~ ("==" | "\uF7D9") ~ expr2 => EqualNode(expr1, expr2)
+//    case expr1 ~ "!=" ~ expr2 => UnequalNode(expr1, expr2)
+//    case expr1 ~ ">" ~ expr2 => GreaterNode(expr1, expr2)
+//    case expr1 ~ (">=" | "≥" | "⩾") ~ expr2 => GreaterEqualNode(expr1, expr2)
+//    case expr1 ~ "<" ~ expr2 => LessNode(expr1, expr2)
+//    case expr1 ~ ("<=" | "≤" | "⩽") ~ expr2 => LessEqualNode(expr1, expr2)
+//  } | span
+//
+//  // TODO: check those
+//  private def horizontalArrowAndVectorOperators: Parser[ASTNode] = equalities
+//  private def diagonalArrowOperators: Parser[ASTNode] = horizontalArrowAndVectorOperators
+//
+//  private def sameQ: Parser[ASTNode] = diagonalArrowOperators ~ ("===" | "=!=") ~ diagonalArrowOperators ^^ {
+//    case expr1 ~ "===" ~ expr2 => SameQNode(expr1, expr2)
+//    case expr1 ~ "=!=" ~ expr2 => UnSameQNode(expr1, expr2)
+//  } | diagonalArrowOperators
+//
+//  private def setRelationOperators: Parser[ASTNode] = sameQ ~ ("∈" | "∉" | "⊂" | "⊃") ~ sameQ ^^ {
+//    case expr1 ~ "∈" ~ expr2 => ElementNode(expr1, expr2)
+//    case expr1 ~ "∉" ~ expr2 => NotElementNode(expr1, expr2)
+//    case expr1 ~ "⊂" ~ expr2 => SubsetNode(expr1, expr2)
+//    case expr1 ~ "⊃" ~ expr2 => SupersetNode(expr1, expr2)
+//  } | sameQ
+//
+//  private def forallAndExists: Parser[ASTNode] = setRelationOperators ~ ("∀" | "∃" | "∄") ~ setRelationOperators ^^ {
+//    case expr1 ~ "∀" ~ expr2 => ForAllNode(expr1, expr2)
+//    case expr1 ~ "∃" ~ expr2 => ExistsNode(expr1, expr2)
+//    case expr1 ~ "∄" ~ expr2 => NotExistsNode(expr1, expr2)
+//  } | setRelationOperators
+//
+//  private def not: Parser[ASTNode] = ("!" | "¬") ~> forallAndExists ^^ {
+//    expr => NotNode(expr)
+//  } | forallAndExists
+//
+//  private def and: Parser[ASTNode] = not ~ ("&&" | "∧" | "⊼") ~ not ^^ {
+//    case expr1 ~ ("&&" | "∧") ~ expr2 => AndNode(expr1, expr2)
+//    case expr1 ~ "⊼" ~ expr2 => NandNode(expr1, expr2)
+//  } | not
+//
+//  private def xor: Parser[ASTNode] = and ~ ("⊻" | "\uF4A2") ~ and ^^ {
+//    case expr1 ~ ("⊻" | "∧") ~ expr2 => XorNode(expr1, expr2)
+//    case expr1 ~ "\uF4A2" ~ expr2 => XnorNode(expr1, expr2)
+//  } | and
+//
+//  private def or: Parser[ASTNode] = xor ~ ("||" | "∨" | "⊽") ~ xor ^^ {
+//    case expr1 ~ ("||" | "∨") ~ expr2 => OrNode(expr1, expr2)
+//    case expr1 ~ ("⊽") ~ expr2 => NorNode(expr1, expr2)
+//  } | xor
+//
+//  private def equivalent: Parser[ASTNode] = (or <~ "⧦") ~ or ^^ {
+//    case expr1 ~ expr2 => EquivalentNode(expr1, expr2)
+//  } | or
+//
+//  private def implies: Parser[ASTNode] = (equivalent <~ ("\uF523" | "⥰")) ~ equivalent ^^ {
+//    case expr1 ~ expr2 => Implies(expr1, expr2)
+//  } | equivalent
+
+//  def tees: Parser[ASTNode] = implies ~ ("⊢" | "⊨" | "⊣" | "⫤" | "⊥" | "⊤") ~ implies ^^ {
+//    case expr1 ~ "⊢" ~ expr2 => RightTeeNode(expr1, expr2)
+//    case expr1 ~ "⊨" ~ expr2 => DoubleRightTeeNode(expr1, expr2)
+//    case expr1 ~ "⊣" ~ expr2 => LeftTeeNode(expr1, expr2)
+//    case expr1 ~ "⫤" ~ expr2 => DoubleLeftTeeNode(expr1, expr2)
+//    case expr1 ~ "⊥" ~ expr2 => UpTeeNode(expr1, expr2)
+//    case expr1 ~ "⊤" ~ expr2 => DownTeeNode(expr1, expr2)
+//  } | implies
+
+  private def root = plusAndMinus
 
   /**
    * Parse the given <code>expression</code> String into an ASTNode.
@@ -100,27 +247,11 @@ class MathematicaParser() extends StdTokenParsers {
    * @return the parsed ASTNode representation of the given formula string
    * @throws SyntaxException
    */
-  def parse(expressionString: String): Any = {
-    expressionString match {
-//      case "-a-b*c!!+d" => "Plus(Plus(Times(-1, a), Times(-1, Times(b, Factorial2(c)))), d)"
-      case "(#^3)&[x][y,z].{a,b,c}" => "Plus(Plus(Times(-1, a), Times(-1, Times(b, Factorial2(c)))), d)"
-      case "a()[0][1]f[[x]]" => "Times(a()[0][1], Part(f, x))"
-      case "a+%%%+%3*4!" => "Plus(Plus(a, Out(-3)), Times(Out(3) shouldEqual Factorial(4)))"
-      case "a+%%%+%3*:=4!" => throw SyntaxException("Syntax error in line: 1 - Operator: := is no prefix operator.\n" + "a+%%%+%3*:=4!\n" + "          ^")
-      case "Integrate(Sin(a_.*x_)^n_IntegerQ, x_Symbol):= -Sin(a*x)^(n-1)*Cos(a*x)/(n*a)+(n-1)/n*Integrate(Sin(a*x)^(n-2),x)/;Positive(n)&&FreeQ(a,x)" => "SetDelayed(Integrate(Power(Sin(Times(a_., x_)), n_IntegerQ), x_Symbol), Condition(Plus(Times(Times(-1, Power(Sin(Times(a, x)), Plus(n, Times(-1, 1)))), Times(Cos(Times(a, x)), Power(Times(n, a), -1))), Times(Times(Plus(n, Times(-1, 1)), Power(n, -1)), Integrate(Power(Sin(Times(a, x)), Plus(n, Times(-1, 2))), x))), And(Positive(n), FreeQ(a, x))))"
-      case "f[[1,2]]" => "Part(f, 1, 2)"
-      case "f[[1]][[2]]" =>"Part(Part(f, 1), 2)"
-      case "f[[1,2,f(x)]]" =>"Part(f, 1, 2, f(x))"
-      case "f[[1]][[2]][[f(x)]]" => "Part(Part(Part(f, 1), 2), f(x))"
-      case "{ArcCsc}[[1]][x]" => "Part(List(ArcCsc), 1)[x]"
-      case "\\[alpha]+\\[alpha]" => "Plus(\\[alpha], \\[alpha])"
-      case "(a+b)[x]" => "Plus(a, b)[x]"
-      case "y'''(x)" => "Derivative(3)[y][x]"
-      case "#^2-3#-1&" => "Function(Plus(Plus(Power(Slot(1), 2), Times(-1, Times(3, Slot(1)))), Times(-1, 1)))"
-      case "++++a+++++2" => "Plus(PreIncrement(PreIncrement(Increment(Increment(a)))), 2)"
-      case _ => realParse(expressionString)
+  def parse(expressionString: String): ASTNode = {
+    val reader = new lexical.Scanner(expressionString)
+    phrase(root)(reader) match {
+      case Success(result, _) => result
+      case noSuccess => throw SyntaxException(noSuccess.toString)
     }
   }
-
-
 }
