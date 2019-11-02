0189ea86aa24a34d2bda005a790d078da2da096d
Tomer Ghelber <tomergelber@gmail.com>
2019-11-02 16:49:59 +0200

    Simple symbol evaluation

--- src/test/scala/tomerghelber/mathematica/eval/MathematicaEvaluatorSpec.scala
+++ src/test/scala/tomerghelber/mathematica/eval/MathematicaEvaluatorSpec.scala
@@ -4,7 +4,9 @@ import org.scalacheck.Arbitrary
 import org.scalatest.{FunSuite, Matchers}
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 import tomerghelber.mathematica._
-import tomerghelber.mathematica.ast.NumberNode
+import tomerghelber.mathematica.ast.{NumberNode, SymbolNode}
+
+import scala.collection.mutable
 
 /**
  * @author user
@@ -14,10 +16,19 @@ class MathematicaEvaluatorSpec extends FunSuite with Matchers with ScalaCheckPro
 
   implicit def arbMathematicaEvaluator: Arbitrary[MathematicaEvaluator] = Arbitrary(mathematicaEvaluatorGen)
   implicit def arbNumberNode: Arbitrary[NumberNode] = Arbitrary(numberNodeGen)
+  implicit def arbSymbolNode: Arbitrary[SymbolNode] = Arbitrary(symbolNodeGen)
 
   test("number evaluated") {
     forAll { (eval: MathematicaEvaluator, expected: NumberNode) =>
       val actual = eval.eval(expected)
+      actual shouldBe expected
+    }
+  }
+
+  test("symbol evaluated") {
+    forAll {(symbol: SymbolNode, expected: NumberNode) =>
+      val eval = new MathematicaEvaluator(mutable.Map((symbol.value, expected)))
+      val actual = eval.eval(symbol)
       actual shouldBe expected
     }
   }
