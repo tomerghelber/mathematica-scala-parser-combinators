6405ac7c3ce984b2166c4a5bfe92eaadfd2899a1
Tomer Ghelber <tomergelber@gmail.com>
2019-11-02 16:39:51 +0200

    Eval with environment

--- src/main/scala/tomerghelber/mathematica/eval/MathematicaEvaluator.scala
+++ src/main/scala/tomerghelber/mathematica/eval/MathematicaEvaluator.scala
@@ -2,15 +2,22 @@ package tomerghelber.mathematica.eval
 
 import tomerghelber.mathematica.ast._
 
+import scala.collection.mutable
+
 /**
  * @author user
  * @since 01-Nov-19
  */
-class MathematicaEvaluator {
+class MathematicaEvaluator(globalEnvironment: mutable.Map[String, ASTNode]= mutable.Map.empty[String, ASTNode]) {
 
   def eval(node: ASTNode): ASTNode = {
+    eval(node, globalEnvironment)
+  }
+
+  private def eval(node: ASTNode, environment: mutable.Map[String, ASTNode]): ASTNode = {
     node match {
       case node: NumberNode => node
+      case SymbolNode(symbol) => environment(symbol)
     }
   }
 }
