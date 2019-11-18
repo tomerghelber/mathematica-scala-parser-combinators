041fc4f7deb7168875ccc196ddbd5f2c898c86d1
Tomer Ghelber <tomergelber@gmail.com>
2019-11-18 23:47:17 +0200

    Added Commutative

---
+++ src/test/scala/com/github/tomerghelber/mathematica/normalform/rules/CommutativeSpec.scala
@@ -0,0 +1,37 @@
+package com.github.tomerghelber.mathematica.normalform.rules
+
+import com.github.tomerghelber.mathematica.ast.{ASTNode, FunctionNode, SymbolNode}
+import com.github.tomerghelber.mathematica.normalform.ASTNodeOrdering
+import com.github.tomerghelber.mathematica.{nodeGen, symbolNodeGen}
+import org.scalacheck.Arbitrary
+import org.scalatest.{FunSpec, Matchers}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+/**
+ * @author user
+ * @since 18-Nov-19
+ */
+class CommutativeSpec extends FunSpec with Matchers with ScalaCheckPropertyChecks {
+  private implicit val symbolNodeArbitrary: Arbitrary[SymbolNode] = Arbitrary(symbolNodeGen)
+  private implicit val nodeArbitrary: Arbitrary[ASTNode] = Arbitrary(nodeGen)
+
+  it("Should not work when symbol is not as function name") {
+    forAll { (name: SymbolNode, arguments: Seq[ASTNode], other: SymbolNode) =>
+      whenever(name != other) {
+        val tested = Commutative(name)
+        val functionNode = FunctionNode(other, arguments)
+        tested.apply(functionNode) shouldBe functionNode
+      }
+    }
+  }
+
+  it("Should be possible if get the same symbol") {
+    forAll { (name: SymbolNode, arguments: Seq[ASTNode]) =>
+      val tested = Commutative(name)
+      val functionNode = FunctionNode(name, arguments)
+      val actual = tested.apply(functionNode)
+      val expected = FunctionNode(name, arguments.sorted(ASTNodeOrdering))
+      actual shouldBe expected
+    }
+  }
+}
