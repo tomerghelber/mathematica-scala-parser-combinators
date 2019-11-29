cf389db8a86a25b505d1f203a8688341b2239886
Tomer Ghelber <tomergelber@gmail.com>
2019-11-29 03:28:29 +0200

    Added ApplyTernaryFunctionNode to nodes

--- src/main/scala/com/github/tomerghelber/mathematica/ast/VectorOperationNode.scala
+++ src/main/scala/com/github/tomerghelber/mathematica/ast/VectorOperationNode.scala
@@ -1,8 +1,8 @@
 package com.github.tomerghelber.mathematica.ast
 
-object CrossNode extends ApplyBinaryFunctionNode with UnapplyFunctionNode {
+object CrossNode extends ApplyBinaryFunctionNode with ApplyTernaryFunctionNode with UnapplyFunctionNode {
   protected val name: String = "Cross"
 }
-object DotNode extends ApplyBinaryFunctionNode with UnapplyFunctionNode {
+object DotNode extends ApplyBinaryFunctionNode with ApplyTernaryFunctionNode with UnapplyFunctionNode {
   protected val name: String = "Dot"
 }
