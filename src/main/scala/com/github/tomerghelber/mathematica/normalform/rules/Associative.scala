63495b68ca3fccc963fc3869c8ab5e3a858fda4c
Tomer Ghelber <tomergelber@gmail.com>
2019-11-29 02:11:07 +0200

    More doc

--- src/main/scala/com/github/tomerghelber/mathematica/normalform/rules/Associative.scala
+++ src/main/scala/com/github/tomerghelber/mathematica/normalform/rules/Associative.scala
@@ -2,7 +2,7 @@ package com.github.tomerghelber.mathematica.normalform.rules
 
 import com.github.tomerghelber.mathematica.ast.{ASTNode, FunctionNode, SymbolNode}
 
-/**
+/** A normal form rule for associative functions.
  * @author user
  * @since 18-Nov-19
  */
@@ -14,7 +14,7 @@ case class Associative(name: SymbolNode) extends NormalFormRuleTemplate {
   protected def work(node: FunctionNode): FunctionNode = {
     val newArgs = node.arguments.flatMap {
       case arg: FunctionNode if arg.name == node.name => arg.arguments
-      case arg: ASTNode => Seq(arg)
+      case arg: ASTNode => Some(arg)
     }
     node.copy(arguments = newArgs)
   }
