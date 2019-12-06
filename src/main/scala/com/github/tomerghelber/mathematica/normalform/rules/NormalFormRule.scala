0de3f3fcf21f8b99bb23bde89775788715e08dfc
Tomer Ghelber <tomerghelber@users.noreply.github.com>
2019-12-06 02:02:48 +0200

    Update NormalFormRule.scala

--- src/main/scala/com/github/tomerghelber/mathematica/normalform/rules/NormalFormRule.scala
+++ src/main/scala/com/github/tomerghelber/mathematica/normalform/rules/NormalFormRule.scala
@@ -2,8 +2,13 @@ package com.github.tomerghelber.mathematica.normalform.rules
 
 import com.github.tomerghelber.mathematica.ast.FunctionNode
 
+trait Rule extends (FunctionNode => FunctionNode)
+trait TwoWayRule extends Rule {
+  def reverse(node: FunctionNode): FunctionNode
+}
+
 /** A rule to convert a `FunctionNode` to its normal form.
  * @author user
  * @since 18-Nov-19
  */
-trait NormalFormRule extends (FunctionNode => FunctionNode)
+trait NormalFormRule extends Rule
